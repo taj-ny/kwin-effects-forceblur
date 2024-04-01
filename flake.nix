127c796f774b24fa604efcbcbc2bc64e86811f7c
taj-ny <79316397+taj-ny@users.noreply.github.com>
2024-04-01 19:59:18 +0200

    flake: move package to package.nix

--- flake.nix
+++ flake.nix
@@ -13,40 +13,6 @@
       inherit system;
     };
   in {
-    packages.default = pkgs.kdePackages.callPackage (
-      { lib
-      , stdenv
-      , cmake
-      , extra-cmake-modules
-      , kwin
-      , wrapQtAppsHook
-      , qttools
-      , fetchFromGitHub
-      }:
-        
-      stdenv.mkDerivation rec {
-        pname = "kwin-effects-forceblur";
-        version = "1.1.2";
-
-        src = ./.;
-
-        nativeBuildInputs = [
-          cmake
-          extra-cmake-modules
-          wrapQtAppsHook
-        ];
-
-        buildInputs = [
-          kwin
-          qttools
-        ];
-        
-        meta = with lib; {
-          description = "A fork of the KWin Blur effect for KDE Plasma 6 with the ability to blur any window on Wayland and X11";
-          license = licenses.gpl3;
-          homepage = "https://github.com/taj-ny/kwin-effects-forceblur";
-        };
-      }
-    ) { };
+    packages.default = pkgs.kdePackages.callPackage ./package.nix { };
   });
 }
---
+++ package.nix
@@ -0,0 +1,33 @@
+{ lib
+, stdenv
+, cmake
+, extra-cmake-modules
+, kwin
+, wrapQtAppsHook
+, qttools
+, fetchFromGitHub
+}:
+
+stdenv.mkDerivation rec {
+  pname = "kwin-effects-forceblur";
+  version = "1.1.2";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    cmake
+    extra-cmake-modules
+    wrapQtAppsHook
+  ];
+
+  buildInputs = [
+    kwin
+    qttools
+  ];
+
+  meta = with lib; {
+    description = "A fork of the KWin Blur effect for KDE Plasma 6 with the ability to blur any window on Wayland and X11";
+    license = licenses.gpl3;
+    homepage = "https://github.com/taj-ny/kwin-effects-forceblur";
+  };
+}
