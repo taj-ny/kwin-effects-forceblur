0111137ab22955eecf9b854295137cf90ad9ff0a
DaddelZeit <daddelzeit.music@gmail.com>
2025-07-20 12:59:31 +0200

    limit edge size to half the minimum dimensions should they be smaller

--- src/blur.cpp
+++ src/blur.cpp
@@ -1115,7 +1115,7 @@ void BlurEffect::blur(BlurRenderData &renderInfo, const RenderTarget &renderTarg
         m_upsamplePass.shader->setUniform(m_upsamplePass.halfpixelLocation, halfpixel);
 
         if (m_refractionStrength > 0) {
-            m_upsamplePass.shader->setUniform(m_upsamplePass.edgeSizePixelsLocation, m_settings.refraction.edgeSizePixels);
+            m_upsamplePass.shader->setUniform(m_upsamplePass.edgeSizePixelsLocation, std::min(m_settings.refraction.edgeSizePixels, (float)std::min(deviceBackgroundRect.width()/2, deviceBackgroundRect.height()/2)));
             m_upsamplePass.shader->setUniform(m_upsamplePass.refractionStrengthLocation, m_refractionStrength);
             m_upsamplePass.shader->setUniform(m_upsamplePass.refractionNormalPowLocation, m_settings.refraction.refractionNormalPow);
             m_upsamplePass.shader->setUniform(m_upsamplePass.refractionRGBFringingLocation, m_refractionRGBFringing);
